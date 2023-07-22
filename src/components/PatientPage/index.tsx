0b29a1b99db604479a211e9a54b584d04bfcb6ae
Markus <markus.becks@gmail.com>
2023-07-22 22:17:42 +0300

    up to 9.24 done

--- src/components/PatientPage/index.tsx
+++ src/components/PatientPage/index.tsx
@@ -1,22 +1,38 @@
 import { FemaleSharp, MaleSharp, TransgenderSharp } from '@mui/icons-material';
 import { Typography } from '@mui/material';
-import patients from '../../services/patients';
-import { Gender, Patient } from '../../types';
+import patientService from '../../services/patients';
+import diagnoseService from '../../services/diagnoses';
+import { Gender, Patient, Diagnosis } from '../../types';
 import { useParams } from 'react-router-dom';
 import { useState, useEffect } from 'react';
+import EntryItem from './EntryItem';
 
 const PatientPage = () => {
 	const [patient, setPatient] = useState<Patient | null>(null);
+	const [diagnoses, setDiagnoses] = useState<Diagnosis[]>([]);
 	const { id } = useParams<{ id: string }>();
 
 	useEffect(() => {
 		// only fetch patient data when id is not undefined
 		if (id) {
 			const getPatient = async () => {
-				const patientData = await patients.get(id);
-				setPatient(patientData);
+				try {
+					const patientData = await patientService.get(id);
+					setPatient(patientData);
+				} catch (error) {
+					console.error('error fetching patient', error);
+				}
 			};
 			getPatient();
+			const getDiagnoses = async () => {
+				try {
+					const diagnosesData = await diagnoseService.getAll();
+					setDiagnoses(diagnosesData);
+				} catch (error) {
+					console.error('error fetching diagnoses', error);
+				}
+			};
+			getDiagnoses();
 		}
 	}, [id]);
 
@@ -37,14 +53,19 @@ const PatientPage = () => {
 
 	return patient ? (
 		<>
-			<h2>Patient details</h2>
 			<div>
-				<Typography fontWeight={700}>
+				<Typography fontWeight={700} fontSize={26} mt={4} mb={4}>
 					{patient.name} {getGenderIcon()}
 				</Typography>
 			</div>
 			<div>occupation: {patient.occupation} </div>
 			<div>ssn: {patient.ssn ? patient.ssn : 'nothing to show here'} </div>
+			<h2>entries</h2>
+			<div>
+				{patient.entries.map((entry) => (
+					<EntryItem key={entry.id} entry={entry} diagnoses={diagnoses} />
+				))}
+			</div>
 		</>
 	) : (
 		<div>Loading patient data...</div>
